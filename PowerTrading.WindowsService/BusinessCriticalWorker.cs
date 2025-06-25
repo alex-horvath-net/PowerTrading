a6bd0245bef6681d663213c09e613389ab78e239
Aladár Horváth <aladar.horvath@outlook.com>
2025-06-25 13:27:15 +0100

    WS improvements

--- PowerTrading.WindowsService/BusinessCriticalWorker.cs
+++ PowerTrading.WindowsService/BusinessCriticalWorker.cs
@@ -12,8 +12,7 @@ namespace PowerTrading.WindowsService {
         private readonly ITime _time;
 
         private readonly ConcurrentQueue<Tuple<Guid, DateTime>> _RunPlans = new();
-        private readonly SemaphoreSlim _avoidOverlappingLoopsFirstGate = new(1, 1);
-        private readonly SemaphoreSlim _avoidOverlappingLoopsSeccondGate = new(1, 1);
+        private readonly SemaphoreSlim _processingStartLock = new(1, 1);
 
         private Task? _processRunPlansTask;
         public Task? ProcessingTask => _processRunPlansTask;
@@ -30,8 +29,16 @@ namespace PowerTrading.WindowsService {
 
         public override async Task StopAsync(CancellationToken cancellationToken) {
             _logger.LogInformation("Service stopping...");
+            if (_processRunPlansTask != null) {
+                try {
+                    await _processRunPlansTask.WaitAsync(cancellationToken);
+                } catch (OperationCanceledException) {
+                    _logger.LogInformation("Processing task cancelled during shutdown.");
+                }
+            }
             await base.StopAsync(cancellationToken);
         }
+
 
         protected override async Task ExecuteAsync(CancellationToken token) {
             _logger.LogInformation("Execution loop starting...");
@@ -44,7 +51,7 @@ namespace PowerTrading.WindowsService {
                 while (!token.IsCancellationRequested) {
                     (runId, runTime) = PlaceANewRunPlan();
 
-                    await FierAndForgetProcessRunPlansTaskWithoutOerlaping(token);
+                    await FireAndForgetProcessRunPlansTaskWithoutOverlapping(token);
                     // this comment line might run on new thread, and hits almost imadiatelly,
                     // because the internal _processRunPlansTask is started, but not waited.
 
@@ -66,18 +73,24 @@ namespace PowerTrading.WindowsService {
             var runId = Guid.NewGuid();
             var runTime = _time.GetTime();
             _RunPlans.Enqueue(Tuple.Create(runId, runTime)); // mutiple threads can enqueue safely
+            if (_RunPlans.Count > 10) { // example threshold, adjust as needed
+                _logger.LogWarning("Backlog size is high: {BacklogCount} scheduled runs pending processing.", _RunPlans.Count);
+            }
             _logger.LogInformation("Scheduled run enqueued for RunTime: {RunTime} RunId: {RunId} ", runTime, runId);
+
+
             return (runId, runTime);
         }
 
-        private async Task FierAndForgetProcessRunPlansTaskWithoutOerlaping(CancellationToken token) {
-            var firstGateIsOpen = false;
-            var secondGateIsOpen = false;
+        private async Task FireAndForgetProcessRunPlansTaskWithoutOverlapping(CancellationToken token) {
+            var isStarted = false;
 
             try {
-                firstGateIsOpen = await _avoidOverlappingLoopsFirstGate.WaitAsync(60 * 60 * 1000, token);
-                if (!firstGateIsOpen) {
-                    _logger.LogWarning("Skipping processing due first gate is cloed, because of overlapping execution.");
+                _logger.LogDebug("Waiting to enter processing start lock...");
+                isStarted = await _processingStartLock.WaitAsync(60 * 60 * 1000, token);
+                _logger.LogDebug("Entered processing start lock.");
+                if (!isStarted) {
+                    _logger.LogWarning("Skipping processing due overlapping execution.");
                     return; // Skip processing if already running
                 }
 
@@ -87,27 +100,30 @@ namespace PowerTrading.WindowsService {
                     return; // Skip processing if already running
                 }
 
-                secondGateIsOpen = await _avoidOverlappingLoopsSeccondGate.WaitAsync(0, token);
-                if (!secondGateIsOpen) {
-                    _logger.LogWarning("Skipping processing due seccond gate is cloed, because of overlapping execution.");
-                    return; // Skip processing if already running
+                if (token.IsCancellationRequested) {
+                    _logger.LogInformation("Cancellation requested before starting processing task.");
+                    return;
                 }
 
-
                 // fier and forget the processing task
-                // there is no need to await here, we want to start processing in the background
-                _processRunPlansTask = ProcessRunPlans(token);
+                // this approach allows the service to continue scheduling new runs without waiting for processing to complete,
+                _processRunPlansTask = ProcessRunPlans(token).ContinueWith(t =>
+                {
+                    if (t.IsFaulted) {
+                        _logger.LogError(t.Exception, "Processing task faulted");
+                    }
+                }, TaskContinuationOptions.OnlyOnFaulted);
+
 
             } finally {
                 // Release the semaphore only if we acquired it
                 // This ensures we don't leave it locked if we never entered the processing
                 // loop due to overlapping execution.
 
-                if (firstGateIsOpen)
-                    _avoidOverlappingLoopsFirstGate.Release();
-
-                if (secondGateIsOpen)
-                    _avoidOverlappingLoopsSeccondGate.Release();
+                if (isStarted) {
+                    _processingStartLock.Release();
+                    _logger.LogDebug("Released processing start lock.");
+                }
             }
         }
 
@@ -121,10 +137,11 @@ namespace PowerTrading.WindowsService {
                     _logger.LogInformation("Starting work for RunTime {RunTime} RunId: {RunId} ", runTime, runId);
 
                     await WorkAsync(runId, runTime, token);
+                    token.ThrowIfCancellationRequested();
 
                     _logger.LogInformation("Completed work for RunTime {RunTime} RunId: {RunId} ", runTime, runId);
                 } catch (OperationCanceledException) {
-                    _logger.LogWarning("Work cancelled.");
+                    _logger.LogWarning("Work cancelled.RunTime {RunTime} RunId: {RunId} ", runTime, runId);
                     throw; // Propagate cancellation
                 } catch (Exception ex) {
                     _logger.LogError(ex, "Error processing scheduled RunTime {RunTime} RunId: {RunId} ", runTime, runId);
@@ -136,8 +153,7 @@ namespace PowerTrading.WindowsService {
 
         public override void Dispose() {
             base.Dispose();
-            _avoidOverlappingLoopsFirstGate?.Dispose();
-            _avoidOverlappingLoopsSeccondGate?.Dispose();
+            _processingStartLock?.Dispose();
         }
     }
 }
